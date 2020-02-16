6234b47d3c0549dd50fe273a54bb8e68d1d26744
lymnyx <58519597+lymnyx@users.noreply.github.com>
2020-02-16 13:34:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.querySelector('.time').innerHTML = Date.now();
+
 setInterval(function() {document.querySelector('.time').innerHTML = Date.now();},10);
+
 document.querySelector('.copybtn').addEventListener('click', function() {
 document.querySelector('#copyinput').value = Date.now();
+});
+
+document.querySelector('.timebtn').addEventListener('click', function() {
+	timecode = new Date(Number(document.querySelector('.timeinput').value));
+	document.querySelector('#converttimeh4').innerHTML = timecode.toTimeString();
 });
